79bbd93a817219090a11def0c0e629fe4a2a752f
james-eo <jameseokonkwo@gmail.com>
2024-03-01 10:30:09 +0100

    Removes a node from AVL tree

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -1,15 +1,48 @@
 #include "binary_trees.h"
 
 /**
- * avl_remove - removes a node from an AVL tree
- *
- * @root: pointer to the root node of
- * the tree for removing a node
- * @value: value to remove in the tree
- * Return: pointer to the new root node of the
- * tree after removing the desired value, and after rebalancing
+ * avl_remove - Removes a node from an AVL tree
+ * @root: Pointer to the root node of the tree
+ * @value: Value to remove from the tree
+ * Return: Pointer to the new root node of the tree
  */
 avl_t *avl_remove(avl_t *root, int value)
 {
+	avl_t *temp;
+
+	if (root == NULL)
+		return (NULL);
+	if (value < root->n)
+		root->left = avl_remove(root->left, value);
+	else if (value > root->n)
+		root->right = avl_remove(root->right, value);
+	else
+	{
+		if (root->left == NULL || root->right == NULL)
+		{
+			temp = root->left ? root->left : root->right;
+			if (temp == NULL)
+			{
+				temp = root;
+				root = NULL;
+			}
+			else
+			{
+				*root = *temp;
+			}
+			free(temp);
+		}
+		else
+		{
+			avl_t *temp = root->right;
+
+			while (temp->left != NULL)
+				temp = temp->left;
+			root->n = temp->n;
+			root->right = avl_remove(root->right, temp->n);
+		}
+	}
+	if (root == NULL)
+		return (NULL);
 	return (root);
 }
